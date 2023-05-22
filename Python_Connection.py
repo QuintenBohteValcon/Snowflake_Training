133bf23567df907ef8374c5ba689ae9cf8026796
QuintenBohteValcon <109140127+QuintenBohteValcon@users.noreply.github.com>
2023-05-22 09:17:17 +0200

    Update Python_Connection.py

--- Python_Connection.py
+++ Python_Connection.py
@@ -2,8 +2,10 @@
 from snowflake.snowpark import Session
 from snowflake.snowpark import DataFrame
 from snowflake.snowpark import functions as fc
+from snowflake.snowpark import Column as cl
 from snowflake.snowpark import Table
 from snowflake.snowpark import DataFrameWriter
+from snowflake.snowpark import types as tp
 import pandas as pd
 import numpy as np
 import os
@@ -26,71 +28,31 @@ def create_session():
 
     return New_Session
 
+def Transformations(df, country_list):
+
+    df_pivot = df.unpivot("Value", "Year", df.columns[3:25]).filter(fc.in_([df.col("COUNTRY_NAME")], country_list))
+
+    df_pivot = df_pivot.select(df_pivot.col("COUNTRY_NAME"), \
+                            df_pivot.col("Indicator_Name"), \
+                            df_pivot.columns()
+                            df_pivot.col("Value").cast(tp.DecimalType(15,2)).name("Value"), \
+                            df_pivot.col("Year").substr(6,4).name("Year"))
+
+    return df_pivot
+
+def Write_df_to_SF(df, Table_Name):
+
+    df.write.mode("overwrite").save_as_table(Table_Name)
+
+
 #%%
 New_Session = create_session()
 
-#%%
-
 df = New_Session.table("Raw_GDP_Data")
-#%%
-
-#%%
-count = 0
-
-year_column = []
-
-year = 1999
-
-for i in range(1, 23):
-
-    year =+  year + 1
-
-    year_column.append(year)
-    
-year_df = New_Session.create_dataframe(year_column, schema=["year"])
-
-column = year_df.select(year_df.col("year"))
-
-
-#%%
-year_df1 = year_df.with_column('year1', year_df["year"])
-
-year_df1.show()
-#%%
 
 country_list = ["Netherlands", 'Poland', 'Italy']
 
-count = 0
+df = Transformations(df, country_list)
 
-for country in country_list:
-
-    count += 1
-
-    df_pivot = df.filter((df.col("COUNTRY_NAME") == country)).unpivot("Value", "Year", df.columns[3:25])
-
-    df_pivot = df_pivot.select(df_pivot.col('Country_Name'), df_pivot.col('Indicator_Name'), df_pivot.col('Value'))
-
-    df_pivot = df_pivot.with_column('year', fc.lit('test'))
-    
-    if count == 1:
-    
-        GDP_Dataframe = df_pivot
-
-    else:
-        GDP_Dataframe = DataFrame.union_all(GDP_Dataframe, df_pivot)
-
-#%%
-
-GDP_DataFrame_pd = GDP_Dataframe.toPandas()
-
-GDP_Dataframe.show()
-
-#%%
-
-GDP_Dataframe.write.mode("overwrite").save_as_table("GDP_Dataframe")
-
-#%%
-
-
-
+Write_df_to_SF(df, 'GDP_Data')
 
