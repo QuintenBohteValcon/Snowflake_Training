581d8e3ddb4390c38180e499a5e5191597563267
QuintenBohteValcon <109140127+QuintenBohteValcon@users.noreply.github.com>
2023-06-02 10:18:34 +0200

    sync

--- Q1.3 - Transformations.py
+++ Q1.3 - Transformations.py
@@ -5,6 +5,7 @@ from snowflake.snowpark import functions as fc
 from snowflake.snowpark import Column as cl
 from snowflake.snowpark import Table
 from snowflake.snowpark import Row
+from snowflake.snowpark import CaseExpr as cw
 from snowflake.snowpark import DataFrameWriter
 from snowflake.snowpark import types as tp
 import pandas as pd
@@ -23,7 +24,7 @@ def create_session():
     "role": "accountadmin",  # optional
     "warehouse": "COMPUTE_WH",  # optional
     "database": "SNOWFLAKE_BURST",  # optional
-    "schema": "CLEAN_DATA",  # optional
+    "schema": "RAW_DATA",  # optional
     }
 
     New_Session = Session.builder.configs(connection_parameters).create()
@@ -31,36 +32,125 @@ def create_session():
     return New_Session
 
 
-def Transformations(df1, df2):
-    Orderlines = df1
-    Stores = df2
-    Orderlines = OrL.select(OrL.col("Store_ID"), fc.year(OrL.col("Order_Date")).name("Year"), OrL.col("Unit_Price"), OrL.col("Currency"))
-    Orderlines = Orderlines.group_by(Orderlines.col("Year"), Orderlines.col("Store_ID"), Orderlines.col("Currency")) \
-                .agg(fc.sum(Orderlines.col("Unit_Price")).name("Amount")).filter((Orderlines.col("Store_ID")) == 'ST01')
+def Transformations(Orderlines, Currency, Stores):
+
+    #Select necesarry columns + create YearMonth_ID column
+    Orderlines = Orderlines.select(Orderlines.col("StoreID").name("Store_ID"), \
+                            fc.year(Orderlines.col("OrderDate")).name("Year"), \
+                            Orderlines.col("UnitPrice"), \
+                            Orderlines.col("CurrencyID").name("Currency_ID"), \
+                            fc.left(Orderlines.col('OrderDate').cast(tp.StringType()), 7).name('YearMonth_ID'))
+
+    #Join Currency table + store table
+    Orderlines = Orderlines.join(Currency, \
+                                (Orderlines.col("YearMonth_ID") == Currency.col("YEARMONTHID")) & \
+                                (Orderlines.col("Currency_ID") == Currency.col("CurrencyID")),  how = "left" )
+
     Orderlines = Orderlines.join(Stores, Orderlines.col("STORE_ID") == Stores.col("STOREID"), how = "left" )
-    Orderlines = Orderlines.select(Orderlines.col("Year"), Orderlines.col("Country"), Orderlines.col("Currency"), Orderlines.col("Amount"))
-    
+
+    #convert currencies to euro
+    Orderlines = Orderlines.with_column('UnitPriceEuro',\
+                            fc.when(Orderlines.col("Currency_ID") == 'EUR', Orderlines.col("UnitPrice"))
+                            .otherwise((Orderlines.col("UnitPrice") / Orderlines.col("CurrencyRate"))))
+        
+    #Group by 
+    Orderlines = Orderlines.group_by(Orderlines.col("Year"), \
+                                    Orderlines.col("Store_ID"), \
+                                    Orderlines.col("Country"))\
+                            .agg(fc.sum(Orderlines.col("UnitPriceEuro")).name("Sales"))
+
+    #select final columns
+    Orderlines = Orderlines.select(Orderlines.Year, \
+                                Orderlines.Country, \
+                                    Orderlines.Sales, \
+                                    fc.lit("EUR").name("Currency"))\
+                            .order_by(Orderlines.Country, Orderlines.Year)
     return Orderlines
 
 def Write_df_to_SF(df, Table_Name):
 
     df.write.mode("overwrite").save_as_table(Table_Name)
 
+
+
 #%%
 New_Session = create_session()
-OrL = New_Session.table("FACTORDERLINES")
-Stores = New_Session.table("STORES")
+Orderlines = New_Session.table("FACTORDERLINES")
+Currency = New_Session.table("FactCurrencyRates")
+Stores = New_Session.table("DimStore")
+
+Sales = Transformations(Orderlines, Currency, Stores)
+Write_df_to_SF(Sales, "Sales_Over_Time")
+
+
+
+
 #%%
-Orderlines = Transformations(OrL, Stores)
+# The Snowpark package is required for Python Worksheets. 
+# You can add more packages by selecting them using the Packages control and then importing them.
 
-Write_df_to_SF(Orderlines, "Sales_Over_Time")
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import col
+from snowflake.snowpark import Session
+from snowflake.snowpark import DataFrame
+from snowflake.snowpark import functions as fc
+from snowflake.snowpark import Column as cl
+from snowflake.snowpark import Table
+from snowflake.snowpark import Row
+from snowflake.snowpark import DataFrameWriter
+from snowflake.snowpark import types as tp
+
+def Transformations(Orderlines, Currency, Stores):
+    
+    #Select necesarry columns + create YearMonth_ID column
+    Orderlines = Orderlines.select(Orderlines.col("StoreID").name("Store_ID"), \
+                            fc.year(Orderlines.col("OrderDate")).name("Year"), \
+                            Orderlines.col("UnitPrice"), \
+                            Orderlines.col("CurrencyID").name("Currency_ID"), \
+                            fc.left(Orderlines.col('OrderDate').cast(tp.StringType()), 7).name('YearMonth_ID'))
+
+    #Join Currency table + store table
+    Orderlines = Orderlines.join(Currency, \
+                                (Orderlines.col("YearMonth_ID") == Currency.col("YEARMONTHID")) & \
+                                (Orderlines.col("Currency_ID") == Currency.col("CurrencyID")),  how = "left" )
+
+    Orderlines = Orderlines.join(Stores, Orderlines.col("STORE_ID") == Stores.col("STOREID"), how = "left" )
+
+    #convert currencies to euro
+    Orderlines = Orderlines.with_column('UnitPriceEuro',\
+                            fc.when(Orderlines.col("Currency_ID") == 'EUR', Orderlines.col("UnitPrice"))
+                            .otherwise((Orderlines.col("UnitPrice") / Orderlines.col("CurrencyRate"))))
+        
+    #Group by 
+    Orderlines = Orderlines.group_by(Orderlines.col("Year"), \
+                                    Orderlines.col("Store_ID"), \
+                                    Orderlines.col("Country"))\
+                            .agg(fc.sum(Orderlines.col("UnitPriceEuro")).name("Sales"))
+
+    #select final columns
+    Orderlines = Orderlines.select(Orderlines.Year, \
+                                Orderlines.Country, \
+                                    Orderlines.Sales, \
+                                    fc.lit("EUR").name("Currency"))\
+                            .order_by(Orderlines.Country, Orderlines.Year)
+    
+    return Orderlines
+
+def Write_df_to_SF(df, Table_Name):
+    df.write.mode("overwrite").save_as_table(Table_Name)
 
 
+def main(session: snowpark.Session): 
 
+    Orderlines = session.table("FACTORDERLINES")
 
+    Currency = session.table("FactCurrencyRates")
+    
+    Stores = session.table("DimStore")
+    
+    Orderlines = Transformations(Orderlines, Currency, Stores)
+    
+    Orderlines.show()
 
-
-
-
-
-# %%
+    Write_df_to_SF(Orderlines, "Sales_Over_Time")
+    
--- Q2.2 - GDP_Transformations+Cleaning.py
+++ Q2.2 - GDP_Transformations+Cleaning.py
@@ -21,23 +21,31 @@ def create_session():
     "role": "accountadmin",  # optional
     "warehouse": "COMPUTE_WH",  # optional
     "database": "SNOWFLAKE_BURST",  # optional
-    "schema": "RAW_SNEAKER_FAQTORY_DATA",  # optional
+    "schema": "RAW_DATA",  # optional
     }
 
     New_Session = Session.builder.configs(connection_parameters).create()
 
     return New_Session
 
-def Transformations(df, country_list):
+def Transformations(GDP, Currency, country_list):
 
-    df_pivot = df.unpivot("Value", "Year", df.columns[3:25]).filter(fc.in_([df.col("COUNTRY_NAME")], country_list))
+    GDP_up = GDP.unpivot("Value", "Year", GDP.columns[3:25]).filter(fc.in_([GDP.col("COUNTRY_NAME")], country_list))
 
-    df_pivot = df_pivot.select(df_pivot.col("COUNTRY_NAME"), \
-                            df_pivot.col("Indicator_Name"), \
-                            df_pivot.col("Value").cast(tp.DecimalType(15,2)).name("Value"), \
-                            df_pivot.col("Year").substr(6,4).name("Year"))
+    GDP_up = GDP_up.select(GDP_up.col("COUNTRY_NAME"), \
+                            GDP_up.col("Indicator_Name"), \
+                            GDP_up.col("Value").cast(tp.DecimalType(15,2)).name("Value"), \
+                            GDP_up.col("Year").substr(6,4).name("Year"))
+
+
+
+
+
+    df_pivot = df_pivot.with_column("Value_Euro", )
 
     return df_pivot
+
+
 
 def Write_df_to_SF(df, Table_Name):
 
@@ -47,11 +55,94 @@ def Write_df_to_SF(df, Table_Name):
 #%%
 New_Session = create_session()
 
-df = New_Session.table("Raw_GDP_Data")
+GDP = New_Session.table("Raw_GDP_Data")
+Currency = New_Session.table("FactCurrencyRates")
+
+Currency.show()
+#%%
 
 country_list = ["Netherlands", 'Poland', 'Italy']
+
+
+GDP_up = GDP.unpivot("Value", "Year", GDP.columns[3:25]).filter(fc.in_([GDP.col("COUNTRY_NAME")], country_list))
+
+
+GDP_up = GDP_up.select(GDP_up.col("COUNTRY_NAME"), \
+                        GDP_up.col("Indicator_Name"), \
+                        GDP_up.col("Value").cast(tp.DecimalType(15,2)).name("Value"), \
+                        GDP_up.col("Year").substr(6,4).name("Year"))
+
+
+Currency_year = Currency.select(Currency.YEARMONTHID).cast()
+
+group_by(fc.year(Currency.col("Year")).name("year"), \
+                                  Currency.CURRENCYID,)  \
+                        .avg(fc.sum(Currency.col("CURRENCYRATE")).name("CURRENCYRATE"))
+
+
+Currency_year.show()
+
+#%%
+
+
+df_pivot = df_pivot.with_column("Value_Euro", )
 
 df = Transformations(df, country_list)
 
 Write_df_to_SF(df, 'GDP_Data')
+
+
+
+#%%
+
+
+
+
+# The Snowpark package is required for Python Worksheets. 
+# You can add more packages by selecting them using the Packages control and then importing them.
+
+import snowflake.snowpark as snowpark
+from snowflake.snowpark import Session
+from snowflake.snowpark import DataFrame
+from snowflake.snowpark import functions as fc
+from snowflake.snowpark import Column as cl
+from snowflake.snowpark import Table
+from snowflake.snowpark import DataFrameWriter
+from snowflake.snowpark import types as tp
+from snowflake.snowpark.functions import col
+
+
+
+def Transformations(df, country_list):
+    
+    df = df.unpivot("Value", "Year", df.columns[3:25]).filter(fc.in_([df.col("COUNTRY_NAME")], country_list))
+
+    df = df.select(df.col("COUNTRY_NAME"), \
+                            df.col("Indicator_Name"), \
+                            df.col("Value").cast(tp.DecimalType(15,2)).name("Value"), \
+                            df.col("Year").substr(6,4).name("Year"))
+    return df
+
+
+
+def Write_df_to_SF(df, Table_Name):
+
+    df.write.mode("overwrite").save_as_table(Table_Name)
+
+
+def main(session: snowpark.Session): 
+    
+    dataframe = session.table('SNOWFLAKE_BURST.RAW_DATA.RAW_GDP_DATA')
+
+    dataframe.show()
+    
+    country_list = ["Netherlands", 'Poland', 'Italy']
+    
+    df = Transformations(dataframe, country_list)
+
+    df.show()
+    
+    Write_df_to_SF(df , 'GDP_Data')
+
+
 
